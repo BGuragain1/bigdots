f53314b3f6931b3f05021d07e7ade39a79b43f45
BGuragain1 <bigyanguragain01@gmail.com>
2025-06-26 12:40:44 +0545

    Update : Modify the cursor

--- .config/nvim/lua/plugins/smearcursor.lua
+++ .config/nvim/lua/plugins/smearcursor.lua
@@ -1,12 +1,12 @@
 return {
 	"sphamba/smear-cursor.nvim",
-	event = "VeryLazy", -- Fix capitalization of event name
+	event = "VeryLazy",
 	opts = {
 		hide_target_hack = true,
-		cursor_color = "#00bfff", -- Neon Blue (cleaner on dark themes)
-		stiffness = 0.6, -- Snappier cursor
-		trailing_stiffness = 0.15, -- Faster trail catch-up
-		trailing_exponent = 3, -- Shorter trail
-		gamma = 1.1, -- Slight contrast pop
+		cursor_color = "#00bfff",
+		stiffness = 0.6,
+		trailing_stiffness = 0.15,
+		trailing_exponent = 3,
+		gamma = 1.1,
 	},
 }
